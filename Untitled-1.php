aa189cc39a6688a06c455fa263760736fbc57e52
rajmallesh <rajmallesh@gmail.com>
2011-03-08 17:54:46 +0530

    untitled 1 commit

---
+++ Untitled-1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body style="text-align:center">
+<table border="1" width="500" height="500" align="center">
+<td style="text-align:center">
+<form id="" action="Untitled-4.php" method="post">
+        	<p>User Name: <input type="text" name="name"  /></p>
+            <p>Password: <input type="password" name="pass" /></p>
+            <p><input type="submit" name="timbus" value="Submit" /></p>
+</form>
+</td>
+</table>
+</body>
+</html>
